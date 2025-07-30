7c6644927e381f18662ccb0018bf2961aec503d0
Nico Waser <nico.waser@venusnet.ch>
2025-07-30 11:34:29 +0200

    Added Context for shared variables

--- Arduino_Mega/Arduino_Mega.ino
+++ Arduino_Mega/Arduino_Mega.ino
@@ -1,28 +1,19 @@
+#include "context.h"
 #include "mqtt_interface.h"
 
 #define ESP_01_SERIAL Serial1
 
+Context context;
 MQTT_Interface mqttInterface(ESP_01_SERIAL);
-
-// test callback function
-void testCallback(const String& topic, const String& payload) {
-    Serial.print("Received on topic: ");
-    Serial.print(topic);
-    Serial.print(" with payload: ");
-    Serial.println(payload);
-}
 
 void setup() {
     // Initialize serial communication
     Serial.begin(115200);
     ESP_01_SERIAL.begin(115200);
-
-    mqttInterface.subscribe("test", testCallback);
-    mqttInterface.publish("test", "Hello, MQTT!");
 }
 
 void loop() {
-    // Call the loop method to handle incoming messages
+    // listen for incoming messages
     mqttInterface.loop();
 
     // Add a small delay to avoid flooding the serial output
---
+++ Arduino_Mega/headers/context.h
@@ -0,0 +1,18 @@
+/*
+    The Context manages variables shared between different modules.
+*/
+#pragma once
+#include <Arduino.h>
+
+struct Context {
+    // force stop
+    bool  force_stop = false;
+
+    // healthcheck variables
+    bool  good_health_check = false;
+    byte  health_check_results[6];
+
+    // movement target
+    float target_rotation[6];
+    bool  target_gripper_state; // true = open, false = closed
+}
